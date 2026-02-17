f7577de8362d3a8ec67d025df34dbd8d49cd4282
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-02-17 00:52:33 +0000

    Create a program to demo sort function

---
+++ 1. Introducción JS/sort.mjs
@@ -0,0 +1,18 @@
+let meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio',
+             'julio', 'agosto', 'septiembre', 'octubre',
+             'noviembre', 'diciembre'];
+
+meses.sort();
+console.log(meses);
+console.log(meses);
+
+let nums = [45, 280, 1467, 2, 19_999];
+
+function comparaNumeros(a, b) {
+  return a - b;
+}
+
+console.log(nums);
+nums.sort(comparaNumeros);
+console.log(nums);
+
