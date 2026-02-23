3909efe1db7eac3ce0d9fb43350d46c4c50007a9
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-02-23 23:59:20 +0000

    Add code for last version of capitalize

--- 2. Callbacks/capitalize.mjs
+++ 2. Callbacks/capitalize.mjs
@@ -1,14 +1,23 @@
 import fs from 'fs';
 
 function main() {
+  console.log('Inicio main');
   fs.readFile('pablito.txt', 'utf-8', (err, data) => {
     if (err) {
       console.error('Hubo un error');
       console.error(err);
       process.exit(1);
     }
-    console.log(data.toUpperCase());
+    fs.writeFile('PABLITO.txt', data.toUpperCase(), err => {
+      if (err) {
+        console.log('No se pudo escribir el archivo');
+        console.log(err);
+        process.exit(1);
+      }
+      console.log('¡Archivo creado exitosamente!');
+    });
   });
+  console.log('Final main');
 }
 
 main();
