dd80d968ee597b642805ae3744b95544e650da8c
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-03-03 01:00:57 +0000

    Add code to demo the creation of new endpoints

--- 4. Express/ejemplo1/app.mjs
+++ 4. Express/ejemplo1/app.mjs
@@ -8,6 +8,42 @@ app.get('/', (req, res) => {
   res.json('¡Hola, mundo!');
 });
 
+const daysOfWeek = [
+  "domingo",
+  "lunes",
+  "martes",
+  "miércoles",
+  "jueves",
+  "viernes",
+  "sábado"
+];
+
+app.get('/fecha', (req, res) => {
+  const rightNow = new Date();
+  res.json({
+    year: rightNow.getFullYear(),
+    month: rightNow.getMonth(),
+    day: rightNow.getDate(),
+    dayOfWeek: daysOfWeek[rightNow.getDay()],
+    hour: rightNow.getHours(),
+    minute: rightNow.getMinutes(),
+    second: rightNow.getSeconds()
+  });
+});
+
+app.get('/fibonacci/:num', (req, res) => {
+  const num = parseInt(req.params.num) || 0;
+  const result = [0, 1];
+  while (result.length < num) {
+    result.push(result.at(-1) + result.at(-2));
+  }
+  res.json({
+    n: num,
+    values: result.slice(0, num),
+    message: 'life is good!'
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server listening at http://${ ipAddress }:${ port }`);
 });
